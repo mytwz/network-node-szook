ce79eacf22857e663dd0a48d9073e3b2c1170087
summer.li <asd995633088@qq.com>
2021-03-23 16:12:39 +0800

    提交

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@
  * @LastEditors: Summer
  * @Description:
  * @Date: 2021-03-19 10:27:21 +0800
- * @LastEditTime: 2021-03-23 16:02:12 +0800
+ * @LastEditTime: 2021-03-23 16:12:29 +0800
  * @FilePath: /network-node-szook/test/index.js
  */
 var ZookServer = require("../");
@@ -13,7 +13,7 @@ var app = new ZookServer({
     signKey: "10.9.16.24",
     redis: {
         prefix: 'im',
-        host: '10.7.17.3',
+        host: '127.0.0.1',
         port: '6379',
         password: "40a7ccf0-5af7-45ec-b7de-b2fe1c6ade83"
     },
